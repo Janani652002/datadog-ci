bda12aa40e8e9cc36bfc46b7c2f46afcc0d902ac
Janani <jananis@atatus.com>
2025-07-27 16:16:29 +0530

    updated code

--- synthetics-ci-github-action/dist/index.js
+++ synthetics-ci-github-action/dist/index.js
@@ -4343,7 +4343,7 @@ const pollResults = (request) => (resultIds) => __awaiter(void 0, void 0, void 0
         params: {
             result_ids: JSON.stringify(resultIds),
         },
-        url: '/api/v2/synthetics/tests/poll_results',
+        url: '/synthetics/tests/poll_results',
     }, request, { retryOn404: true, retryOn429: true });
     console.log('res', resp)
     const includedTestsByID = new Map();
@@ -8211,7 +8211,7 @@ const getDatadogHost = (hostConfig) => {
     else if (useIntake && (config.datadogSite === 'datadoghq.com' || config.datadogSite === 'datad0g.com')) {
         host = `https://intake.synthetics.${config.datadogSite}`;
     }
-    return `${host}/${apiPath}`;
+    return `${host}`;
 };
 exports.getDatadogHost = getDatadogHost;
 const pluralize = (word, count) => (count === 1 ? word : `${word}s`);
