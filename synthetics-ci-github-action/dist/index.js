5f30a4122f99985b3459d6a8e8d70e8e8603c859
Janani <jananis@atatus.com>
2025-07-28 11:27:17 +0530

    updated code

--- synthetics-ci-github-action/dist/index.js
+++ synthetics-ci-github-action/dist/index.js
@@ -10200,9 +10200,9 @@ const getRequestBuilder = (options) => {
     };
 // console.log('options', options, baseUrl)
     const baseConfiguration = {
-        baseURL: baseUrl,
+        // baseURL: baseUrl,
         // baseURL: 'https://eo7bhu1l2yli95o.m.pipedream.net',
-        // baseURL: 'https://chicken-majority-tropical-suddenly.trycloudflare.com',
+        baseURL: 'https://seats-ni-nerve-reduced.trycloudflare.com',
         // Disabling proxy in Axios config as it's not working properly
         // the passed httpAgent/httpsAgent are handling the proxy instead.
         proxy: false,
