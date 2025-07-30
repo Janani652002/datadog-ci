e1532a67b96d2235b4e2eaf6ce1746303593d863
Janani <jananis@atatus.com>
2025-07-30 13:07:29 +0530

    debug

--- synthetics-ci-github-action/dist/index.js
+++ synthetics-ci-github-action/dist/index.js
@@ -4334,7 +4334,7 @@ const pollResults = (request) => (resultIds) => __awaiter(void 0, void 0, void 0
         params: {
             result_ids: JSON.stringify(resultIds),
         },
-        url: '/synthetics/tests/poll_results',
+        url: '/synthetic/tests/poll_results',
         // url: '/continuous_testing/synthetic/tests/poll_results',
     }, request, { retryOn404: true, retryOn429: true });
     const includedTestsByID = new Map();
